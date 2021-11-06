2e65a339404d5dd28cd76dcf6d5613ca9682d9ef
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-11-05 19:47:14 -0700

    Add a title and some basic text

--- start.pl
+++ start.pl
@@ -3,9 +3,15 @@
 :- use_module(library(http/http_server)).
 
 home_page(_Request) :-
+    PageTitle = 'Pokedex served with Prolog',
     reply_html_page(
-        title('Demo server'),
-        [ h1('Hello world!')]).
+        title(PageTitle),
+        [
+            h1(PageTitle),
+            p(['This page serves up facts about pokemon using ', 
+                a([href='https://pokeapi.co'], 'PokeAPI'), '.'])
+        ]
+    ).
 
 :- http_handler(
     root(.),
@@ -13,4 +19,5 @@ home_page(_Request) :-
     []
 ).
 :- http_handler(root(home), home_page, []).
+
 :- initialization http_server([port(8080)]).
