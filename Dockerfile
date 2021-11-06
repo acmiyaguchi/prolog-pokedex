895ac466eb59459e4be8ae5179f9ff4b1e3da911
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-11-05 21:26:21 -0700

    Add inotify to reload the server when source changes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM swipl
 
+WORKDIR /app
+RUN apt-get update && apt-get -y install build-essential
+COPY packages.pl .
+RUN swipl packages.pl
+
 RUN useradd -ms /bin/bash www
 USER www
 COPY . /app
--- start.pl
+++ start.pl
@@ -1,6 +1,6 @@
 % https://stackoverflow.com/questions/36966024/running-prolog-on-a-server-from-php-or-make-it-daemon
-
 :- use_module(library(http/http_server)).
+:- use_module(library(inotify)).
 
 home_page(_Request) :-
     PageTitle = 'Pokedex served with Prolog',
@@ -8,7 +8,7 @@ home_page(_Request) :-
         title(PageTitle),
         [
             h1(PageTitle),
-            p(['This page serves up facts about pokemon using ', 
+            p(['This page serves up facts about pokemon using ',
                 a([href='https://pokeapi.co'], 'PokeAPI'), '.'])
         ]
     ).
@@ -20,4 +20,31 @@ home_page(_Request) :-
 ).
 :- http_handler(root(home), home_page, []).
 
-:- initialization http_server([port(8080)]).
+% Watch a directory using inotify
+% https://www.swi-prolog.org/pack/file_details/inotify/prolog/inotify.pl?show=src
+reload_source(Dir) :-
+    inotify_init(INotify, []),
+    inotify_add_watch(INotify, Dir, [all]),
+    repeat,
+        (   inotify_read_event(INotify, Ev, [])
+        ->
+            (
+                Ev = modify(file(File)),
+                split_string(File, ".", "", L),
+                last(L, "pl")
+                  ->
+                    writeln([File, 'was changed']),
+                    make
+            ),
+            fail
+        ;   format('Timeout~n'),
+            !
+        ).
+
+run :-
+    % first load then server on port 8080
+    http_server([port(8080)]),
+    % watch prolog files in the current directory for whether to restart the server
+    reload_source('/app').
+
+:- initialization(run).
